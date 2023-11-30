b8915fdbe203f81f9d60bb13837b6e286b98ed21
Matt Venn <matt@mattvenn.net>
2023-11-30 12:19:43 +0100

    change interface names to better fit with caravel

--- wb_buttons_leds.v
+++ wb_buttons_leds.v
@@ -6,71 +6,70 @@
     licensed under the GPL
 */
 `default_nettype none
-
 module wb_buttons_leds #(
-    parameter   [31:0]  BASE_ADDRESS    = 32'h03000000,        // base address
+    parameter   [31:0]  BASE_ADDRESS    = 32'h3000_0000,        // base address
     parameter   [31:0]  LED_ADDRESS     = BASE_ADDRESS,
     parameter   [31:0]  BUTTON_ADDRESS  = BASE_ADDRESS + 4
     ) (
-    input wire          clk,
-    input wire          reset,
+    input wire          wb_clk_i,
+    input wire          wb_rst_i,
 
     // wb interface
-    input wire          i_wb_cyc,       // wishbone transaction
-    input wire          i_wb_stb,       // strobe - data valid and accepted as long as !o_wb_stall
-    input wire          i_wb_we,        // write enable
-    input wire  [31:0]  i_wb_addr,      // address
-    input wire  [31:0]  i_wb_data,      // incoming data
-    output reg          o_wb_ack,       // request is completed 
-    output wire         o_wb_stall,     // cannot accept req
-    output reg  [31:0]  o_wb_data,      // output data
+    input wire          wbs_cyc_i,       // wishbone transaction
+    input wire          wbs_stb_i,       // strobe - data valid and accepted as long as !wbs_sta_o
+    input wire          wbs_we_i,        // write enable
+    input wire  [31:0]  wbs_adr_i,      // address
+    input wire  [31:0]  wbs_dat_i,      // incoming data
+    output reg          wbs_ack_o,       // request is completed 
+    output wire         wbs_sta_o,     // cannot accept req
+    output reg  [31:0]  wbs_dat_o,      // output data
 
     // buttons
-    input wire  [2:0]   buttons,
-    output reg  [7:0]   leds
+    input wire  [7:0]   buttons,
+    output reg  [7:0]   leds,
+    output wire [15:0]   oeb
     );
 
-    assign o_wb_stall = 0;
-
-    initial leds = 8'b0;
+    assign wbs_sta_o = 0;
+    assign oeb = 16'b0000000011111111;
 
     // writes
-    always @(posedge clk) begin
-        if(reset)
+    always @(posedge wb_clk_i) begin
+        if(wb_rst_i)
             leds <= 8'b0;
-        else if(i_wb_stb && i_wb_cyc && i_wb_we && !o_wb_stall && i_wb_addr == LED_ADDRESS) begin
-            leds <= i_wb_data[7:0];
+        else if(wbs_stb_i && wbs_cyc_i && wbs_we_i && !wbs_sta_o && wbs_adr_i == LED_ADDRESS) begin
+            leds <= wbs_dat_i[7:0];
         end
     end
 
     // reads
-    always @(posedge clk) begin
-        if(i_wb_stb && i_wb_cyc && !i_wb_we && !o_wb_stall)
-            case(i_wb_addr)
+    always @(posedge wb_clk_i) begin
+        if(wbs_stb_i && wbs_cyc_i && !wbs_we_i && !wbs_sta_o)
+            case(wbs_adr_i)
                 LED_ADDRESS: 
-                    o_wb_data <= {24'b0, leds};
+                    wbs_dat_o <= {24'b0, leds};
                 BUTTON_ADDRESS: 
-                    o_wb_data <= {29'b0, buttons};
+                    wbs_dat_o <= {24'b0, buttons};
                 default:
-                    o_wb_data <= 32'b0;
+                    wbs_dat_o <= 32'b0;
             endcase
     end
 
     // acks
-    always @(posedge clk) begin
-        if(reset)
-            o_wb_ack <= 0;
+    always @(posedge wb_clk_i) begin
+        if(wb_rst_i)
+            wbs_ack_o <= 0;
         else
             // return ack immediately
-            o_wb_ack <= (i_wb_stb && !o_wb_stall && (i_wb_addr == LED_ADDRESS || i_wb_addr == BUTTON_ADDRESS));
+            wbs_ack_o <= (wbs_stb_i && !wbs_sta_o && (wbs_adr_i == LED_ADDRESS || wbs_adr_i == BUTTON_ADDRESS));
     end
 
 `ifdef FORMAL
-	default clocking @(posedge clk); endclocking
-	default disable iff (reset);
+    default clocking @(posedge wb_clk_i); endclocking
+    default disable iff (wb_rst_i);
 
-    cyc:    assume property (i_wb_cyc |=> i_wb_cyc && o_wb_ack);
-	write:  cover property (##1 $rose(i_wb_stb) |-> ##[+] o_wb_data[3:0] == 4'b1010);
-    read:   cover property (##1 $rose(i_wb_stb) |-> ##[+] leds[7:0] == 8'b11110000);
+    cyc:    assume property (wbs_cyc_i |=> wbs_cyc_i && wbs_ack_o);
+    write:  cover property (##1 $rose(wbs_stb_i) |-> ##[+] wbs_dat_o[3:0] == 4'b1010);
+    read:   cover property (##1 $rose(wbs_stb_i) |-> ##[+] leds[7:0] == 8'b11110000);
 `endif
 endmodule
